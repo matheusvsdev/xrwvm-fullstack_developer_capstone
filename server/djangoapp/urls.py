940393bd226f91a3014f786963dff065c717a0e0
matheusvsdev <matheusvaldevino1997@gmail.com>
2025-03-23 19:03:42 -0400

    Module3.2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,10 +12,15 @@ urlpatterns = [
     path('login', views.login_user, name='login'),
     path('logout/', views.logout_request, name='logout'),
     path('register/', views.registration, name='register'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path('get_cars', view=views.get_cars, name ='getcars'),
+    path('get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
+    path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
 
     # path for dealer reviews view
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
+    path('add_review', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
